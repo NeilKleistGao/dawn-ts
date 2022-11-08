ccf2a15736a46392b8d142574681bbeefe281367
NeilKleistGao <neilkleist@gmail.com>
2022-11-08 13:21:48 +0800

    Add tests

--- test/tsc_parsing.test.ts
+++ test/tsc_parsing.test.ts
@@ -4,4 +4,20 @@ test("calling tsc api", () => {
   let code = dawn.code<number>("2 + 2");
   expect(code.run() === 4);
 })
+
+test("parsing test", () => {
+  {
+    let code = dawn.code<string>(`"abcdefg.substring(2)"`);
+    expect(code.run() === "cdefg");
+  }
   
+  {
+    let code = dawn.code<boolean>(`true && (false || true)`);
+    expect(code.run());
+  }
+
+  {
+    let code = dawn.code<null>(`null`);
+    expect(code.run() === null);
+  }
+})
