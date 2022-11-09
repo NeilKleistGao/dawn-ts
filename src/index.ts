e96f45ed2316b63271c9f089a12d9a998791c615
NeilKleistGao <neilkleist@gmail.com>
2022-11-09 20:28:18 +0800

    Update tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as ts from "typescript";
-import * as dawnts from "./transformer";
+import { dawn } from "./transformer";
 
 export default(p_program: ts.Program): ts.TransformerFactory<ts.Node> =>
   (p_ctx) => {
     const checker = p_program.getTypeChecker();
-    const transformer = new dawnts.dawn.QuasiQuotationTransformer(p_ctx, checker);
+    const transformer = new dawn.QuasiQuotationTransformer(p_ctx, checker);
     return (p_sf) => {
       return transformer.run(p_sf as ts.SourceFile);
     };
